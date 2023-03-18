8092cba68094f7da260e9303d75e74eaa71e4d53
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-03-18 16:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 //your JS code here. If required.
-function updateOutput() {
-  // Create a promise that resolves with "Hello, world!" after 1 second
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Hello, world!");
-    }, 1000);
-  });
-
-  // Update the text of the HTML element with ID "output" when the promise resolves
-  promise.then((result) => {
-    document.getElementById("output").textContent = result;
-  });
+function Hello(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);
+	});
 }
+Hello().then(data=>{
+	document.getElementById("document").textContent=data;
+});
